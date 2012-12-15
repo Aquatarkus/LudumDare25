f7f0c02e6f9abc3a688ca115a58a1ee87c5d3019
Alex Loret de Mola <EdgarVerona@gmail.com>
2012-12-15 14:05:25 -0800

    Added a first pass at the Fart class.
    Needs to be given the current stage object so that it can spawn its
    fartling trail.

---
+++ js/fart.js
@@ -0,0 +1,93 @@
+
+(function(game, createjs) {
+
+	function Fart() {
+		this.initialize();
+	}
+
+	Fart.prototype = new createjs.Container();
+	
+	Fart.prototype.Container_initialize = Fart.prototype.initialize;
+
+	Fart.prototype.initialize = function() {
+		this.Container_initialize();
+		
+		this.shape = new createjs.Shape();
+		this.makeShape();
+		this.alpha = 1.0;
+		
+		this.addChild(this.shape);
+	};
+	
+	Fart.prototype.stationary = false;
+	Fart.prototype.vX = 0;
+	Fart.prototype.vY = 0;
+	Fart.prototype.spawnRate = 200;
+	Fart.prototype.lastSpawned = 0;
+	Fart.prototype.fartlings = [];
+	Fart.prototype.stage = null;
+	Fart.prototype.decay = 4000;
+	
+	Fart.prototype.isLive = true;
+	
+	Fart.prototype.makeShape = function() {
+		var g = this.shape.graphics;
+
+		g.clear();
+		
+		g.beginFill("#0F0").drawCircle(0, 0, 16, 16);
+		g.endFill();
+	};
+	
+	
+	Fart.prototype.tick = function() {
+		if (!this.isLive) {
+			return;
+		}
+		
+		var interval = createjs.Ticker.getInterval();
+		var prevX = this.x;
+		var prevY = this.y;
+		
+		var hasMoved = false;
+		
+		if (!this.stationary) {
+			if (this.vX != 0) {
+				this.x = this.x + ((interval / 1000) * this.vX);
+				hasMoved = true;
+			}
+			if (this.vY != 0) {
+				this.y = this.y + ((interval / 1000) * this.vY);
+				hasMoved = true;
+			}
+		}
+		
+		if (hasMoved) {
+			var ticks = interval * createjs.Ticker.getTicks();
+		
+			if (ticks - this.lastSpawned > this.spawnRate) {
+				this.lastSpawned = ticks;
+				
+				var newFartling = new game.Fart();
+				newFartling.stationary = true;
+				newFartling.alpha = 0.5;
+				newFartling.x = prevX;
+				newFartling.y = prevY;
+				
+				// Fart gotta keep moving
+				this.stage.addChild(newFartling);
+			}
+		} else {
+			var newAlpha = this.alpha - (interval / this.decay);
+			
+			if (newAlpha <= 0) {
+				this.alpha = 0;
+				this.isLive = false;
+			} else {
+				this.alpha = newAlpha;
+			}
+		}
+	};
+
+	game.Fart = Fart;
+}(game, createjs));
