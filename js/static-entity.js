3f3ce211c074cabb306b6949755312386a5fef85
Alex Loret de Mola <EdgarVerona@gmail.com>
2012-12-15 02:27:30 -0800

    Test of a simple map approach
    Still needs work - placeholders, all instantiating StaticEntities that
    basically don't do anything.  Ideally each symbol would instantiate an
    object in a particular way, so we can just define maps by text symbols.
    Also dropped in a different easeljs file - for some reason, the one that
    was in there was just HTML?  Looked like Github may have hiccupped.

---
+++ js/static-entity.js
@@ -0,0 +1,30 @@
+
+(function(game, createjs) {
+
+	function StaticEntity() {
+		this.initialize();
+	}
+
+	var e = StaticEntity.prototype = new createjs.Container();
+
+	e.Container_initialize = e.initialize;
+
+	e.initialize = function() {
+		this.Container_initialize();
+		
+		this.shape = new createjs.Shape();
+		
+		this.addChild(this.shape);
+	};
+
+	e.makeShape = function() {
+		var g = this.shape.graphics;
+
+		g.clear();
+		
+		g.beginStroke("#F00").beginFill("#00F").drawRect(0, 0, 32, 32);
+		g.endFill().endStroke();
+	};
+
+	game.StaticEntity = StaticEntity;
+}(game, createjs));
