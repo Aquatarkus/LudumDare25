68697eb06f02a5664abedd2f8b87874b082aa234
John Shaffstall <shaffstall@gmail.com>
2012-12-16 12:25:11 -0800

    shifting fart left

--- js/Fart.js
+++ js/Fart.js
@@ -1,186 +1,185 @@
+// Fart.js
 
+var Fart = function(x, y, direction)
+{
+    Entity.call(this, x, y, direction);
+    this.caresAboutDistance = true;
+};
+Fart.prototype = new CollidableEntity();
+Fart.prototype.constructor = Fart;
 
-    var Fart = function(x, y, direction)
-    {
-        Entity.call(this, x, y, direction);
-        this.caresAboutDistance = true;
-    };
-    Fart.prototype = new CollidableEntity();
-    Fart.prototype.constructor = Fart;
-	
-	Fart.prototype.stationary = false;
-	Fart.prototype.vX = 0;
-	Fart.prototype.vY = 0;
-	Fart.prototype.spawnRate = 200;
-	Fart.prototype.lastSpawned = 0;
-	Fart.prototype.decay = 4000;
-	// the first frame of its creation, count that it has changed tiles as well.
-	Fart.prototype.hasChangedTile = true;
-	
-	Fart.prototype.makeShape = function() {
-		var g = this.shape.graphics;
+Fart.prototype.stationary = false;
+Fart.prototype.vX = 0;
+Fart.prototype.vY = 0;
+Fart.prototype.spawnRate = 200;
+Fart.prototype.lastSpawned = 0;
+Fart.prototype.decay = 4000;
+// the first frame of its creation, count that it has changed tiles as well.
+Fart.prototype.hasChangedTile = true;
 
-		g.clear();
-		
-		g.beginFill("#0F0").drawCircle(16, 16, 16, 16);
-		g.endFill();
-		g.beginStroke("#F00").drawRect(0, 0, TileWidth-1, TileHeight-1);
-	};
-    
-    Fart.prototype.getTileX = function() {
-        return Math.round(this.x / TileWidth);
-    };
-    
-    Fart.prototype.getTileY = function() {
-        return Math.round(this.y / TileHeight);
-    };
-	
-	Fart.prototype.tick = function() {
-    
-        if (!this.stationary) {
-            this.checkDistanceEntities();
+Fart.prototype.makeShape = function() {
+    var g = this.shape.graphics;
+
+    g.clear();
+
+    g.beginFill("#0F0").drawCircle(16, 16, 16, 16);
+    g.endFill();
+    g.beginStroke("#F00").drawRect(0, 0, TileWidth-1, TileHeight-1);
+};
+
+Fart.prototype.getTileX = function() {
+    return Math.round(this.x / TileWidth);
+};
+
+Fart.prototype.getTileY = function() {
+    return Math.round(this.y / TileHeight);
+};
+
+Fart.prototype.tick = function() {
+
+    if (!this.stationary) {
+        this.checkDistanceEntities();
+    }
+
+    var interval = createjs.Ticker.getInterval();
+    var prevX = this.x;
+    var prevY = this.y;
+
+    var newX = this.x;
+    var newY = this.y;
+
+    var hasMoved = false;
+
+    if (!this.stationary) {
+        if (this.vX != 0) {
+            newX = this.x + ((interval / 1000) * this.vX);
+            hasMoved = true;
         }
-		
-		var interval = createjs.Ticker.getInterval();
-		var prevX = this.x;
-		var prevY = this.y;
-        
-        var newX = this.x;
-        var newY = this.y;
-        
-		var hasMoved = false;
-		
-		if (!this.stationary) {
-			if (this.vX != 0) {
-				newX = this.x + ((interval / 1000) * this.vX);
-				hasMoved = true;
-			}
-			if (this.vY != 0) {
-				newY = this.y + ((interval / 1000) * this.vY);
-				hasMoved = true;
-			}
-		}
-        
-        var newTileX = Math.round(newX / TileWidth);
-		var newTileY = Math.round(newY / TileHeight);
-		        
-		if (hasMoved) {
-            
-            if (this.checkCollision(newTileX, newTileY)) {
-            
-				this.hasChangedTile = (newTileX != this.getTileX() || newTileY != this.getTileY());
-			
-                this.x = newX;
-                this.y = newY;
-                var ticks = interval * createjs.Ticker.getTicks();
-            
-                if (ticks - this.lastSpawned > this.spawnRate) {
-                    this.lastSpawned = ticks;
-                    
-                    var newFartling = new Fart();
-                    newFartling.stationary = true;
-                    newFartling.alpha = 0.5;
-                    newFartling.x = prevX;
-                    newFartling.y = prevY;
-                    
-                    // Fart gotta keep moving
-                    addEntity(newFartling);
+        if (this.vY != 0) {
+            newY = this.y + ((interval / 1000) * this.vY);
+            hasMoved = true;
+        }
+    }
+
+    var newTileX = Math.round(newX / TileWidth);
+    var newTileY = Math.round(newY / TileHeight);
+
+    if (hasMoved) {
+
+        if (this.checkCollision(newTileX, newTileY)) {
+
+            this.hasChangedTile = (newTileX != this.getTileX() || newTileY != this.getTileY());
+
+            this.x = newX;
+            this.y = newY;
+            var ticks = interval * createjs.Ticker.getTicks();
+
+            if (ticks - this.lastSpawned > this.spawnRate) {
+                this.lastSpawned = ticks;
+
+                var newFartling = new Fart();
+                newFartling.stationary = true;
+                newFartling.alpha = 0.5;
+                newFartling.x = prevX;
+                newFartling.y = prevY;
+
+                // Fart gotta keep moving
+                addEntity(newFartling);
+            }
+        } else {
+            var entityList = getEntityList(this.getTileX(), this.getTileY());
+            if (entityList) {
+                entityList.push(this);
+            }
+            hasMoved = false;
+            this.vX = 0;
+            this.vY = 0;
+        }
+    }
+
+    if (!hasMoved) {
+        var newAlpha = this.alpha - (interval / this.decay);
+
+        if (newAlpha <= 0) {
+            this.alpha = 0;
+            removeEntity(this);
+        } else {
+            this.alpha = newAlpha;
+        }
+    }
+};
+
+Fart.prototype.handleCollisions = function(entityList) {
+    var result = true;
+
+    for(var i = 0; i < entityList.length; i++) {
+        if (entityList[i].collides && !(entityList[i] instanceof Fart) && !(entityList[i] instanceof Player)) {
+            this.vX = 0;
+            this.vY = 0;
+            result = false;
+        }
+    }
+
+    return result;
+};
+
+Fart.prototype.checkDistanceEntities = function() {
+    if (!this.hasChangedTile) {
+        return;
+    }
+
+    var entities = getDistanceEntities();
+
+    var closestDE = null;
+    var closestDist = 9999;
+
+    var tileX = this.getTileX();
+    var tileY = this.getTileY();
+
+    for(var i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+        var found = false;
+        var dist = 9999;
+
+        var entityTileX = entity.getTileX();
+        var entityTileY = entity.getTileY();
+
+        switch(entity.direction) {
+            case Direction.Up:
+                if (tileX == entityTileX && tileY <= entityTileY) {
+                    found = true;
+                    dist = entityTileY - tileY;
                 }
-            } else {
-                var entityList = getEntityList(this.getTileX(), this.getTileY());
-                if (entityList) {
-                    entityList.push(this);
+                break;
+            case Direction.Down:
+                if (tileX == entityTileX && tileY >= entityTileY) {
+                    found = true;
+                    dist = tileY - entityTileY;
                 }
-                hasMoved = false;
-                this.vX = 0;
-                this.vY = 0;
-            }
-		}
-        
-        if (!hasMoved) {
-			var newAlpha = this.alpha - (interval / this.decay);
-			
-			if (newAlpha <= 0) {
-				this.alpha = 0;
-				removeEntity(this);
-			} else {
-				this.alpha = newAlpha;
-			}
-		}
-	};
-    
-    Fart.prototype.handleCollisions = function(entityList) {
-        var result = true;
-        
-        for(var i = 0; i < entityList.length; i++) {
-            if (entityList[i].collides && !(entityList[i] instanceof Fart) && !(entityList[i] instanceof Player)) {
-                this.vX = 0;
-                this.vY = 0;
-                result = false;
-            }
+                break;
+            case Direction.Left:
+                if (tileY == entityTileY && tileX <= entityTileX) {
+                    found = true;
+                    dist = entityTileX - tileX;
+                }
+                break;
+            case Direction.Right:
+                if (tileY == entityTileY && tileX >= entityTileX) {
+                    found = true;
+                    dist = tileX - entityTileX;
+                }
+                break;
         }
-        
-        return result;
-    };
-    
-    Fart.prototype.checkDistanceEntities = function() {
-        if (!this.hasChangedTile) {
-			return;
-		}
-		
-		var entities = getDistanceEntities();
-		
-        var closestDE = null;
-        var closestDist = 9999;
-        
-        var tileX = this.getTileX();
-        var tileY = this.getTileY();
-        
-        for(var i = 0; i < entities.length; i++) {
-            var entity = entities[i];
-            var found = false;
-            var dist = 9999;
-            
-            var entityTileX = entity.getTileX();
-            var entityTileY = entity.getTileY();
-            
-            switch(entity.direction) {
-                case Direction.Up:
-                    if (tileX == entityTileX && tileY <= entityTileY) {
-                        found = true;
-                        dist = entityTileY - tileY;
-                    }
-                    break;
-                case Direction.Down:
-                    if (tileX == entityTileX && tileY >= entityTileY) {
-                        found = true;
-                        dist = tileY - entityTileY;
-                    }
-                    break;
-                case Direction.Left:
-                    if (tileY == entityTileY && tileX <= entityTileX) {
-                        found = true;
-                        dist = entityTileX - tileX;
-                    }
-                    break;
-                case Direction.Right:
-                    if (tileY == entityTileY && tileX >= entityTileX) {
-                        found = true;
-                        dist = tileX - entityTileX;
-                    }
-                    break;
-            }
-            
-            if (found && closestDist > dist) {
-                closestDist = dist;
-                closestDE = entity;
-            }
+
+        if (found && closestDist > dist) {
+            closestDist = dist;
+            closestDE = entity;
         }
-        
-        if (closestDE) {
-            closestDE.affect(this);
-        }
-    };
-    
-    
-    
+    }
+
+    if (closestDE) {
+        closestDE.affect(this);
+    }
+};
+
+
